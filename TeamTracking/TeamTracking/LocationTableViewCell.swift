bfeb4f44b9e7e1bc523dc3954e49db7a72463429
Diwakar Kamboj <primekrieger@gmail.com>
2016-01-20 20:06:23 +0530

    Custom Location cell added

---
+++ TeamTracking/TeamTracking/LocationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationTableViewCell.swift
+//  TeamTracking
+//
+//  Created by Diwakar Kamboj on 20/01/16.
+//
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
