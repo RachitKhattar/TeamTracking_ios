312f0b6453af2f829cf32defdad12ba2283d8ddf
Diwakar Kamboj <primekrieger@gmail.com>
2016-01-20 20:44:29 +0530

    Add AppSettings and Constants

--- TeamTracking/TeamTracking/AppSettings.swift
+++ TeamTracking/TeamTracking/AppSettings.swift
@@ -10,4 +10,35 @@ import UIKit
 
 class AppSettings: NSObject {
     
+    static let sharedInstance = AppSettings()
+    
+    override init() {
+        super.init()
+        NSUserDefaults.standardUserDefaults().registerDefaults([
+            "userName": "diwakar"
+            ])
+    }
+    
+    var userName: String {
+        get{
+            let key = self.userDefaultForKey("userName")
+            return key as! String
+        }
+        set(value) {
+            self.setUserDefaultValueForKey(value, key: "userName")
+        }
+    }
+    
+    // MARK: Private methods
+    func userDefaultForKey(key: String) -> AnyObject? {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        return defaults.objectForKey(key)
+    }
+    
+    func setUserDefaultValueForKey(value: AnyObject, key: String) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setValue(value, forKey: key)
+        defaults.synchronize()
+    }
+    
 }
---
+++ TeamTracking/TeamTracking/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TeamTracking
+//
+//  Created by Diwakar Kamboj on 20/01/16.
+//
+//
+
+import Foundation
+
+struct Constants {
+    static let host_address = "http://localhost:8585"
+}
