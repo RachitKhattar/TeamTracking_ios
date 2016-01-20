2822080c8a18d02939e8a9ec2c1b54a25f683034
Diwakar Kamboj <primekrieger@gmail.com>
2016-01-20 20:49:39 +0530

    Add RestServiceHelper

---
+++ TeamTracking/TeamTracking/RestServiceHelper.swift
@@ -0,0 +1,31 @@
+//
+//  RestServiceHelper.swift
+//  TeamTracking
+//
+//  Created by Diwakar Kamboj on 20/01/16.
+//
+//
+
+import UIKit
+import Alamofire
+
+class RestServiceHelper: NSObject {
+    
+//    func trackLocation(long: String, lat: String) {
+//        let url = "\(Constants.host_address)/teamtracking/rest/location/tracklocation/\(lat)/\(long)/\(AppSettings.sharedInstance.userName)/"
+//        
+//        Alamofire.request(.GET, url)
+//            .responseJSON { response in
+//                let completionResult = RestResponseModel()
+//                
+//                if let receivedValue = response.result.value {
+//                    completionResult.errorCode = receivedValue.objectForKey("errorCode") as! Int
+//                    completionResult.responseDescription = receivedValue.objectForKey("description")
+//                }
+//                completion(result: completionResult)
+//        }
+//    }
+//    
+//    func
+    
+}
