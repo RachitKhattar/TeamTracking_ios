49642089cbbc5b386cb6756149cf5ecc43073699
Diwakar Kamboj <primekrieger@gmail.com>
2016-01-21 13:12:39 +0530

    Add Icons

--- TeamTracking/TeamTracking/AppDelegate.swift
+++ TeamTracking/TeamTracking/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+//        AppSettings.sharedInstance.userName = "diwakar"
         // Override point for customization after application launch.
         // Register for Remote Notifications
         let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
